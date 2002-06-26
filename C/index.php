6a961c5f1a93974de1548c0bcd47d220242388df
olly <olly>
2002-06-26 12:36:23 +0000

    Added redirector to allow shorter URLs in cvs check-in messages.

---
+++ C/index.php
@@ -0,0 +1,14 @@
+<?php
+list($file, $rev, $rev2) = explode('?', $QUERY_STRING);
+$redirect = 'http://cvs.sf.net/cgi-bin/viewcvs.cgi/xapian/' . $file;
+if ($rev == '') {
+ // deleted file
+} else if ($rev2 == '') {
+ // added file
+ $redirect .= '?rev=' . $rev . '&content-type=text/vnd.viewcvs-markup';
+} else {
+ // updated file
+ $redirect .= '?r1=' . $rev . '&r2=' . $rev2;
+}
+header('Location: ' . $redirect);
+?>
