8a79bed0f9660095ee9796e0ff86031c748cb68a
olly <olly>
2002-09-30 18:32:44 +0000

    Corrected details of which directories ./buildall needs to be run in.

--- cvs.php
+++ cvs.php
@@ -17,8 +17,11 @@ To get the latest version of Xapian directly from our CVS, follow these steps:
 <li> Log in anonymously into the CVS: <tt>cvs -d:pserver:anonymous@cvs.xapian.org:/cvsroot/xapian login</tt>
 (Just hit return when you are asked for the password.)
 <li> Check out the latest version: <tt>cvs -z3 -d:pserver:anonymous@cvs.xapian.org:/cvsroot/xapian co xapian</tt>
-<li> In the newly created <tt>xapian</tt> directory, issue the command <tt>./buildall</tt> - this will run various developer tools to produce a source tree
-like you'd get from unpacking a release source tarball.
+<li> In the newly created <tt>xapian/xapian-core</tt>,
+    <tt>xapian/xapian-examples</tt>, and
+    <tt>xapian/xapian-applications/omega</tt> directories, issue the command
+    <tt>./buildall</tt> - this will run various developer tools to produce a
+    source tree like you'd get from unpacking a release source tarball.
 </ol>
 <p>
 We plan to set up an automatic snapshot system which will try to compile and
