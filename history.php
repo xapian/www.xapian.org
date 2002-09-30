9fcffd73e34c90668414bda21fa51d6e97f2f13f
olly <olly>
2002-09-30 23:36:03 +0000

    Fixed a few typos, and added a paragraph about webtop.

--- history.php
+++ history.php
@@ -9,12 +9,12 @@ print $navbar;
 
 <center><h1>History</h1></center>
 
-The Xapian project is based open the code of Omsee (also know as Open Muscat)
+The Xapian project is based upon the code of Omsee (also known as Open Muscat)
 originally developed and released under the GPL by BrightStation PLC.
 BrightStation is no longer trading, but some of the original developers
 from BrightStation and others from the Omsee community decided to continue
 development of the code.  Before the name Xapian was chosen, the project
-was briefly know as Omseek.<p>
+was briefly known as Omseek.<p>
 
 BrightStation's Omsee project was to be a replacement for their Muscat 3.6
 library, which was originally written by Dr. Martin Porter at Cambridge
@@ -23,6 +23,12 @@ to commercially exploit the technology; the company was soon renamed
 Muscat Ltd when the focus shifted from CDs to the web.  Muscat Ltd was
 bought be Maid PLC, who renamed themselves first to Dialog Corporation, and
 then to BrightStation PLC.<p>
+
+Omsee (using the muscat36 backend) was the retrieval engine which powered
+BrightStation's Webtop search engine, which offered a search over around
+500 million web pages (around 1.5 terrabytes of database files).  Searches
+took less than a second, and including the web crawlers and index building,
+it all ran on 30 Intel boxes.<p>
 
 Xapian is a modern class library, but has evolved out of over 20 years of
 commercial and academic experience.
