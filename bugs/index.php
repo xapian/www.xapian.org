931fd21646e8d6a0341f22d62a080ec7c251ea35
olly <olly>
2002-12-08 03:43:44 +0000

    redirect to bugs.php instead of SF bug tracker

--- bugs/index.php
+++ bugs/index.php
@@ -1,1 +1,3 @@
-<?php header('Location: http://sourceforge.net/tracker/?group_id=35626&atid=414875'); ?>
+<?php header('Location: http://www.xapian.org/bugs.php');
+//http://sourceforge.net/tracker/?group_id=35626&atid=414875
+?>
