7813c71914fcba881cac18f5d4940fab145f4fa0
olly <olly>
2003-10-09 13:12:27 +0000

    Added links to Gmane for each list

--- lists.php
+++ lists.php
@@ -15,10 +15,16 @@
 
 <p>There are three mailing lists relating to Xapian:</p>
 <ul>
-<li> <tt><a href="http://lists.sourceforge.net/mailman/listinfo/xapian-discuss">xapian-discuss</a></tt> for general discussions about the Xapian project. 
-<li> <tt><a href="http://lists.sourceforge.net/mailman/listinfo/xapian-devel">xapian-devel</a></tt> for technical discussions about the development of Xapian. 
-<li> <tt><a href="http://lists.sourceforge.net/mailman/listinfo/xapian-commits">xapian-commits</a></tt> receives CVS commits (can be a <em>lot</em> of messages).
+<li> <tt><a href="http://lists.sourceforge.net/mailman/listinfo/xapian-discuss">xapian-discuss</a></tt> for general discussions about the Xapian project
+[<a href="http://gmane.org/find.php?list=xapian-discuss">Gmane</a>]
+<li> <tt><a href="http://lists.sourceforge.net/mailman/listinfo/xapian-devel">xapian-devel</a></tt> for technical discussions about the development of Xapian 
+[<a href="http://gmane.org/find.php?list=xapian-devel">Gmane</a>]
+<li> <tt><a href="http://lists.sourceforge.net/mailman/listinfo/xapian-commits">xapian-commits</a></tt> receives CVS commits (can be a <em>lot</em> of messages)
+[<a href="http://gmane.org/find.php?list=xapian-cvs">Gmane</a>]
 </ul>
+
+<p>The [Gmane] links take you to the Gmane news gateway which allows you to
+read these list with a newsreader, or browse the archives on the web.</p>
 
 </div>
 
