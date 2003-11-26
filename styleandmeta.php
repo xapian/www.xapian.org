60f0b3b5c64b9297c521ebfe736174257664a49c
olly <olly>
2003-11-26 16:34:16 +0000

    PHP include for stylesheets and meta tags

---
+++ styleandmeta.php
@@ -0,0 +1,8 @@
+<link rel="stylesheet" type="text/css" media="print" href="print.css">
+<!--<link rel="alternate stylesheet" type="text/css" media="screen" title="Menu on the left" href="layout1.css">-->
+<style type="text/css" media="screen">
+<!--
+@import "layout2.css";
+//-->
+</style>
+<meta name="MSSmartTagsPreventParsing" content="true">
