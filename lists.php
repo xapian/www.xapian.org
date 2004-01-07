cd6a017122405b4898124a68f0abae0f46906c5d
olly <olly>
2004-01-07 17:30:57 +0000

    Changed gmane links to go to the info page (which shows a nice list traffic graph).

--- lists.php
+++ lists.php
@@ -16,11 +16,11 @@
 <p>There are three mailing lists relating to Xapian:</p>
 <ul>
 <li> <tt><a href="http://lists.sourceforge.net/mailman/listinfo/xapian-discuss">xapian-discuss</a></tt> for general discussions about the Xapian project
-[<a href="http://gmane.org/find.php?list=xapian-discuss">Gmane</a>]
+[<a href="http://gmane.org/info.php?group=gmane.comp.search.xapian.general">Gmane</a>]
 <li> <tt><a href="http://lists.sourceforge.net/mailman/listinfo/xapian-devel">xapian-devel</a></tt> for technical discussions about the development of Xapian 
-[<a href="http://gmane.org/find.php?list=xapian-devel">Gmane</a>]
+[<a href="http://gmane.org/info.php?group=gmane.comp.search.xapian.devel">Gmane</a>]
 <li> <tt><a href="http://lists.sourceforge.net/mailman/listinfo/xapian-commits">xapian-commits</a></tt> receives CVS commits (can be a <em>lot</em> of messages)
-[<a href="http://gmane.org/find.php?list=xapian-cvs">Gmane</a>]
+[<a href="http://gmane.org/info.php?group=gmane.comp.search.xapian.cvs">Gmane</a>]
 </ul>
 
 <p>The [Gmane] links take you to the Gmane news gateway which allows you to
