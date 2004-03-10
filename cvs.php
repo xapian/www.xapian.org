7230ff106ff6124c2e5e88b5a2f45abdaf0ef581
olly <olly>
2004-03-10 00:22:37 +0000

    Link to the xapian module in the CVS tree, as few people want to look at the website in CVS!

--- cvs.php
+++ cvs.php
@@ -15,7 +15,7 @@
 
 <p>The Xapian CVS tree (including file history from the original Omsee project)
 can be browsed online on the <a
-href="http://cvs.xapian.org/">Xapian CVS server</a>.
+href="http://cvs.xapian.org/xapian/">Xapian CVS server</a>.
 To get the latest version of Xapian directly from our CVS, follow these steps:</p>
 <ol>
 <li> Check out the latest version:
