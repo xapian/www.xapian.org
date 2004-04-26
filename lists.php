57e30620990041f2837b59c3d1408f4f5bf14d35
olly <olly>
2004-04-26 19:58:51 +0000

    Mailing lists are now on lists.xapian.org

--- lists.php
+++ lists.php
@@ -15,11 +15,11 @@
 
 <p>There are three mailing lists relating to Xapian:</p>
 <ul>
-<li> <tt><a href="http://lists.sourceforge.net/mailman/listinfo/xapian-discuss">xapian-discuss</a></tt> for general discussions about the Xapian project
+<li> <tt><a href="http://lists.xapian.org/mailman/listinfo/xapian-discuss">xapian-discuss</a></tt> for general discussions about the Xapian project
 [<a href="http://gmane.org/info.php?group=gmane.comp.search.xapian.general">Gmane</a>]
-<li> <tt><a href="http://lists.sourceforge.net/mailman/listinfo/xapian-devel">xapian-devel</a></tt> for technical discussions about the development of Xapian 
+<li> <tt><a href="http://lists.xapian.org/mailman/listinfo/xapian-devel">xapian-devel</a></tt> for technical discussions about the development of Xapian 
 [<a href="http://gmane.org/info.php?group=gmane.comp.search.xapian.devel">Gmane</a>]
-<li> <tt><a href="http://lists.sourceforge.net/mailman/listinfo/xapian-commits">xapian-commits</a></tt> receives CVS commits (can be a <em>lot</em> of messages)
+<li> <tt><a href="http://lists.xapian.org/mailman/listinfo/xapian-commits">xapian-commits</a></tt> receives CVS commits (can be a <em>lot</em> of messages)
 [<a href="http://gmane.org/info.php?group=gmane.comp.search.xapian.cvs">Gmane</a>]
 </ul>
 
