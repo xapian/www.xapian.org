9ff1575bc91c2ce6be2d6463f4bca3b7c4ff1b37
olly <olly>
2005-05-13 23:38:27 +0000

    0.9.0 released.

--- .scripts/update_website.sh
+++ .scripts/update_website.sh
@@ -12,7 +12,7 @@
 set -e
 
 # FIXME : need to avoid having to update this...
-tarball="/usr/data/www/oligarchy.co.uk/xapian/0.8.5/xapian-core-0.8.5.tar.gz"
+tarball="/usr/data/www/oligarchy.co.uk/xapian/0.9.0/xapian-core-0.9.0.tar.gz"
 
 projectdir="/u1/olly/xapian-website-update"
 cvsdir=":pserver:cvsuser:anonymous@cvs.xapian.org:/usr/data/cvs"
--- download.php
+++ download.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
 "http://www.w3.org/TR/html4/strict.dtd">
 <html>
-<? $version = "0.8.5"; $dversion = "0-8-5"; $branch = "0.8"; ?>
+<? $version = "0.9.0"; $branch = "0.9"; ?>
 <head>
 <title>The Xapian Project : Downloads</title>
 <? include "styleandmeta.php"; ?>
@@ -14,25 +14,22 @@
 <h1>Downloads</h1>
 
 <p>The <? echo $branch ?> branch
-features greatly improved database update speed with quartz, improved
-compression of quartz termlist tables, many improvements to the documentation,
-many portability improvements, and new bindings allowing Xapian to be used
-from Java and TCL.</p>
+features a few API changes, the most notable being a rewritten QueryParser
+which is reentrant, has encapsulated internals, and parses better than the old
+one.  Note that the examples are now a subdirectory of xapian-core, so there
+is no longer a separate xapian-examples download (most of the size of the
+xapian-examples download was due to configure and other generated files!)
+</p>
 
-<p id="<? echo $branch ?>">The latest release is <B><? echo $version ?></B>
-(there's a minor update to xapian-bindings numbered <B>0.8.5.1</B>
-which fixes problems with the PHP and Java bindings):</p>
+<p id="<? echo $branch ?>">The latest release is <B><? echo $version ?></B>:</p>
 
 <ul>
 <li> <A HREF="http://www.oligarchy.co.uk/xapian/<? echo $version ?>/xapian-core-<? echo $version ?>.tar.gz">xapian-core</A>: the Xapian library itself
-<A HREF="http://cvs.xapian.org/xapian/xapian-core/NEWS?rev=v<? echo $dversion ?>">[news]</A>
-<li> <A HREF="http://www.oligarchy.co.uk/xapian/<? echo $version ?>/xapian-examples-<? echo $version ?>.tar.gz">xapian-examples</A>: small example programs
-<A HREF="http://cvs.xapian.org/xapian/xapian-examples/NEWS?rev=v<? echo $dversion ?>">[news]</A>
+<A HREF="http://svn.xapian.org/*checkout*/tags/<? echo $version ?>/xapian-core/NEWS">[news]</A>
 <li> <A HREF="http://www.oligarchy.co.uk/xapian/<? echo $version ?>/omega-<? echo $version ?>.tar.gz">omega</A>: Omega - an application built on Xapian, consisting of indexers and a CGI search frontend.
-<A HREF="http://cvs.xapian.org/xapian/xapian-applications/omega/NEWS?rev=v<? echo $dversion ?>">[news]</A>
-<li> <A HREF="http://www.oligarchy.co.uk/xapian/0.8.5/xapian-bindings-0.8.5.1.tar.gz">xapian-bindings</A>: SWIG and JNI bindings allowing Xapian to be used from various other programming languages
-<!--<A HREF="http://cvs.xapian.org/xapian/xapian-bindings/NEWS?rev=v<? echo $dversion ?>">[news]</A>-->
-<A HREF="http://cvs.xapian.org/xapian/xapian-bindings/NEWS?rev=v0-8-5-1">[news]</A>
+<A HREF="http://svn.xapian.org/*checkout*/tags/<? echo $version ?>/xapian-applications/omega/NEWS">[news]</A>
+<li> <A HREF="http://www.oligarchy.co.uk/xapian/<? echo $version ?>/xapian-bindings-<? echo $version ?>.tar.gz">xapian-bindings</A>: SWIG and JNI bindings allowing Xapian to be used from various other programming languages
+<A HREF="http://svn.xapian.org/*checkout*/tags/<? echo $version ?>/xapian-bindings/NEWS">[news]</A>
 </ul>
 
 <h1 id="deb">Debian packages</h1>
