8d57a64d533534964e47f914f0d3d006d131b0e6
olly <olly>
2005-09-19 23:08:04 +0000

    Gmane search is now live!

--- users.php
+++ users.php
@@ -142,11 +142,10 @@ half a billion webpages using 30 Intel boxes with sub-second retrieval times.
 Unfortunately Webtop's parent company stopped trading in 2001.
 </p>
 
-<p id="gmane">Organisation: Gmane.org (in development)<br>
-Application: Searching archives of mailing lists (Gmane allows these to be viewed as newsgroups)<br>
-Location: <a href="http://rain.gmane.org">rain.gmane.org</a><br>
-Database size: around 22,000,000 messages<br>
-Note:
+<p id="gmane">Organisation: <a href="http://www.gmane.org/">Gmane</a><br>
+Application: Searching archives of mailing lists (Gmane allows these to be viewed as newsgroups, or on the web)<br>
+Location: <a href="http://search.gmane.org">search.gmane.org</a><br>
+Database size: around 28,000,000 messages
 </p>
 
 <p id="daviswiki">Organisation: <a href="http://www.daviswiki.org/">Davis Wiki</a><br>
