bb4d487363c45e394bd04755011aa6b4d9b5acc6
olly <olly>
2006-07-10 02:39:55 +0000

    Update debian package status

--- download.php
+++ download.php
@@ -39,7 +39,10 @@ Note: the latest bindings on CPAN are currently for Xapian 0.9.2, but there are 
 <h1 id="deb">Debian and Ubuntu packages</h1>
 
 <p>We now supply packages for Debian (stable, testing, and unstable) and Ubuntu
-(breezy and dapper).  Here's how to get them.
+(breezy and dapper).  The xapian-core and xapian-omega packages have now been
+accepted into the Debian archive, so if you're using unstable you can install
+them as you would any other Debian package.  If you're not using Debian
+unstable, or need the bindings, here's how to get them.
 </p>
 
 <p>If you're running Debian stable add the following to your sources.list:
@@ -123,12 +126,6 @@ use sudo):
 <span id="prompt">$</span> fakeroot apt-get source -b xapian-bindings xapian-omega
 <span id="prompt">$</span> sudo dpkg -i xapian-omega*.deb python-xapian*.deb
 </pre></blockquote>
-
-<p>
-Any assistance (getting these packages into Debian, reporting problems
-which aren't noted above or in the TODO lists in each package, or
-fixing any of these problems) will be gratefully accepted.
-</p>
 
 <h1 id="RPM">RPM packages</h1>
 
