e5f99ce28e70c1ef4e22ba6d071437a79455c67c
olly <olly>
2006-09-01 04:20:25 +0000

    Tweak wording of front page

--- index.php
+++ index.php
@@ -15,17 +15,18 @@
 
 <p>Welcome to the Xapian project website.</p>
 
-<p>Xapian is an Open Source Probabilistic Information Retrieval library,
-released under the GPL.  It's written in C++, with bindings to allow use
-from Perl, Python, PHP, Java, TCL, C#, and Ruby (so far!)</p>
+<p>Xapian is an Open Source Search Engine Library, released under the GPL.
+It's written in C++, with bindings to allow use
+from Perl, Python, PHP, Java, Tcl, C#, and Ruby (so far!)</p>
 
-<p>Xapian is designed to be a highly adaptable toolkit to allow developers
+<p>Xapian is a highly adaptable toolkit which allows developers
 to easily add advanced indexing and search facilities to their own
-applications.</p>
+applications.  It supports the Probabilistic Information Retrieval
+model and also supports a rich set of boolean query operators.</p>
 
 <p>If you're after a packaged search engine for your website, you
-should take a look at Omega, which is an application we supply built upon
-Xapian.  But unlike most other website search solutions, Xapian's
+should take a look at Omega: an application we supply built upon
+Xapian.  Unlike most other website search solutions, Xapian's
 versatility allows you to extend Omega to meet your needs as they grow.</p>
 
 <p>The <a href="download.php">latest stable version is <?echo $version;?></a>,
