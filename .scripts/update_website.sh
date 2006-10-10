777dda48c34f5638e485399c8b919be7ee5950fd
olly <olly>
2006-10-10 16:44:08 +0000

    Update for 0.9.7.

--- .scripts/update_website.sh
+++ .scripts/update_website.sh
@@ -12,7 +12,7 @@
 set -e
 
 # FIXME : need to avoid having to update this...
-tarball="/usr/data/www/oligarchy.co.uk/xapian/0.9.6/xapian-core-0.9.6.tar.gz"
+tarball="/usr/data/www/oligarchy.co.uk/xapian/0.9.7/xapian-core-0.9.7.tar.gz"
 
 projectdir="/u1/olly/xapian-website-update"
 #cvsdir=":pserver:cvsuser:anonymous@cvs.xapian.org:/usr/data/cvs"
--- version.php
+++ version.php
@@ -1,1 +1,1 @@
-<? $version = "0.9.6"; $branch = "0.9"; $release_date = "2006-05-15"; ?>
+<? $version = "0.9.7"; $branch = "0.9"; $release_date = "2006-10-10"; ?>
