2f8d641104519741e019f429dd33b77b13d81f25
olly <olly>
2007-10-11 12:31:25 +0000

    Merge with the history recently removed from the xapian-core README.

--- history.php
+++ history.php
@@ -16,25 +16,51 @@
 <p>Xapian is a modern class library, but has evolved out of over 25 years of
 commercial and academic experience.</p>
 
-<p>The Xapian project is based upon the code of Open Muscat (also known as
-Omsee) originally developed and released under the GPL by BrightStation PLC.
-BrightStation is no longer trading, but some of the original developers from
-BrightStation and others from the Open Muscat community decided to continue
-development of the code.  Before the name Xapian was chosen, the project was
-briefly known as Omseek.</p>
+<p>
+Xapian is partly derived from the Open Muscat engine, developed by BrightStation
+PLC and released under the
+<a href="http://www.opensource.org/licenses/gpl-license.php">GPL</a>.
+Open Muscat was built to be a replacement for
+the proprietary Muscat 3.6 information retrieval system, which was written
+almost entirely in <a href="http://en.wikipedia.org/wiki/BCPL">BCPL</a>,
+and becoming hard to extend in the ways they wanted.
+</p>
 
-<p>BrightStation's Open Muscat project was to be a replacement for the
-Muscat 3.6 information retrieval system,
-which was originally written by Dr. Martin Porter at Cambridge
-University.  In 1984, Martin and John Snyder founded Cambridge CD Publishing
+<p>
+Muscat 3.6 was originally written by
+<a href="http://en.wikipedia.org/wiki/Martin_Porter">Dr. Martin Porter</a>
+at Cambridge University.  In 1984, Martin and John Snyder founded Cambridge CD
+Publishing
 to commercially exploit the technology; the company was soon renamed
 Muscat Ltd when the focus shifted from CDs to the web.  Muscat Ltd was
 bought by Maid PLC, who renamed themselves first to Dialog Corporation, and
 then to BrightStation PLC (when they sold the Dialog brand and content
-to Thomson).</p>
+to Thomson).
+</p>
 
-<p>Open Muscat (using the muscat36 backend) was the retrieval engine which powered
-BrightStation's Webtop search engine
+<p>
+In early 2001, BrightStation's management renamed Open Muscat to Omsee, and
+shortly afterwards they sadly announced they were taking development
+closed-source.
+</p>
+
+<p>
+A number of developers (both former BrightStation employees and others from the
+fledgling Open Muscat community) took the last GPLed version and have been
+continuing development under the GPL.  This project was initially known as
+OmSeek, but BrightStation complained this was too close to their (untrademarked
+and unpublicised) name Omsee.  It was simpler to change it than to argue, and
+the name Xapian was chosen.
+</p>
+
+<p>
+The last numbered BrightStation release was Open Muscat 0.4.1, although there
+was extensive development in CVS after that.  The first official Xapian release
+was 0.5.0.
+</p>
+
+<p>Open Muscat (using the muscat36 backend) was the retrieval engine which
+powered BrightStation's Webtop search engine
 (<a href="http://web.archive.org/web/20000708023501/http://www.webtop.com/index.html">archive.org snapshot</a>),
 which offered a search over around
 500 million web pages (around 1.5 terabytes of database files).  Searches
