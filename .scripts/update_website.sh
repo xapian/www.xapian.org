6daf12b3b3d9b2dacec06827ecf72924295f2809
olly <olly>
2008-03-18 00:16:29 +0000

    Fix directory omindex looks for website in Don't fail just because there's no existing search index

--- .scripts/update_website.sh
+++ .scripts/update_website.sh
@@ -167,9 +167,11 @@ rm -rf "${tmpdir}"
 # FIXME: sort out permissions so this'll work for other people too
 rm -rf /home/olly/omega/data/default.tmp /home/olly/omega/data/default.old
 
-/home/olly/install/bin/omindex --db /home/olly/omega/data/default.tmp --url / /usr/data/www/xapian.org
+/home/olly/install/bin/omindex --db /home/olly/omega/data/default.tmp --url / "$htmldir"
 
-mv /home/olly/omega/data/default /home/olly/omega/data/default.old
+if test -d /home/olly/omega/data/default ; then
+  mv /home/olly/omega/data/default /home/olly/omega/data/default.old
+fi
 mv /home/olly/omega/data/default.tmp /home/olly/omega/data/default
 # keep it just in case! rm -rf /home/olly/omega/data/default.old
 
