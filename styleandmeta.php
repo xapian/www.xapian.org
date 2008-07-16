7b583cae9b051deb6d4c1ecbb5a16a8006c4e91a
olly <olly>
2008-07-16 02:56:40 +0000

    Fixes from HTML validation

--- styleandmeta.php
+++ styleandmeta.php
@@ -1,8 +1,9 @@
-<link rel="stylesheet" type="text/css" media="print" href="print.css">
-<!--<link rel="alternate stylesheet" type="text/css" media="screen" title="Menu on the left" href="layout1.css">-->
+<link rel="stylesheet" type="text/css" media="print" href="print.css" />
+<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+<link rel="icon" href="/favicon.ico" type="image/x-icon" />
 <style type="text/css" media="screen">
 <!--
 @import "layout2.css";
 //-->
 </style>
-<meta name="MSSmartTagsPreventParsing" content="true">
+<meta name="MSSmartTagsPreventParsing" content="true" />
