ed2e47ea259806762094c33fdc1733f2ce64c5a1
olly <olly>
2008-12-24 04:49:54 +0000

    Update for 1.0.10

--- download.php
+++ download.php
@@ -34,9 +34,9 @@ complete overview of the changes</a> in the 1.0 release.
 <a HREF="http://svn.xapian.org/*checkout*/tags/<? echo $version ?>/xapian-applications/omega/NEWS">[news]</a>
 <li> <a HREF="http://oligarchy.co.uk/xapian/<? echo $version ?>/xapian-bindings-<? echo $version ?>.tar.gz">xapian-bindings</a>: SWIG and JNI bindings allowing Xapian to be used from various other programming languages
 <a HREF="http://svn.xapian.org/*checkout*/tags/<? echo $version ?>/xapian-bindings/NEWS">[news]</a>
-<li> <a HREF="http://oligarchy.co.uk/xapian/1.0.8/Search-Xapian-1.0.8.0.tar.gz">Search::Xapian</a>: Perl bindings
-(<a HREF="http://search.cpan.org/~olly/Search-Xapian-1.0.8.0/">on CPAN</a>)
-<a HREF="http://search.cpan.org/~olly/Search-Xapian-1.0.8.0/Changes">[news]</a> <small>(CPAN mirrors may not have updated yet)</small>
+<li> <a HREF="http://oligarchy.co.uk/xapian/1.0.10/Search-Xapian-1.0.10.0.tar.gz">Search::Xapian</a>: Perl bindings
+(<a HREF="http://search.cpan.org/~olly/Search-Xapian-1.0.10.0/">on CPAN</a>)
+<a HREF="http://search.cpan.org/~olly/Search-Xapian-1.0.10.0/Changes">[news]</a> <small>(CPAN mirrors may not update for a few days after a new release)</small>
 </ul>
 
 <p>The wiki contains a <a href="http://wiki.xapian.org/ReleaseNotes">summary of bugs, patches, and workarounds</a>
