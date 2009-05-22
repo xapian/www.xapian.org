861b91ac98a250cab30791736f9c02215ece3c1d
olly <olly>
2009-05-22 13:52:03 +0000

    Add SVN over WebDAV instructions.

--- bleeding.php
+++ bleeding.php
@@ -63,9 +63,94 @@ now in the tree checked out by the above command.
 </p>
 
 <?php if ($_SERVER['QUERY_STRING'] != "") { ?>
+
+<h2>Write Access</h2>
+
 <p>
-If you have write access, you need to do a small amount of configuration
-as we use userv to provide additional security.
+We provide write access in one of two ways, either using WebDAV over https or
+over ssh (generally you'll be given one sort of access <b>or</b> the other, not
+both).  There's a section describing how to set up for each below.
+</p>
+
+<h3>WebDAV over https</h3>
+
+<p>
+If your username is <i>fred</i> then execute the following command:
+</p>
+
+<blockquote><tt>
+svn --username fred ls https://svn-dav.xapian.org:8443/xapian/
+</tt></blockquote>
+
+<p>
+The certificate we use for HTTPS is issued by <a href="http://www.cacert.org/"
+>CAcert</a> so unless your subversion client is already configured to trust
+such certificates you will get the following warning:
+</p>
+
+<blockquote><pre>
+Error validating server certificate for 'https://svn.xapian.org:8443':
+ - The certificate hostname does not match.
+Certificate information:
+ - Hostname: svn-dav.xapian.org
+ - Valid: from Sun, 19 Apr 2009 03:48:43 GMT until Tue, 19 Apr 2011 03:48:43 GMT
+ - Issuer: http://www.CAcert.org, CAcert Inc.
+ - Fingerprint: 2a:72:69:71:fd:da:dc:97:0c:f5:de:2a:73:8b:f5:78:51:26:ba:72
+(R)eject, accept (t)emporarily or accept (p)ermanently?
+</pre></blockquote>
+
+<p>
+If you do, verify the fingerprint is as above.  If the fingerprint
+doesn't match, or you have other concerns, talk to the other developers
+and don't just accept the certificate.  If you're happy with it, you
+can either accept it temporarily (and be asked to confirm it for every
+"session", though not every remote svn operation), or permanently.  We
+recommend you accept it permanently as otherwise you need to recheck
+the fingerprint repeatedly which makes it more likely you might accidentally
+accept a spoofed certificate (so press <i>p</i>).
+</p>
+
+<p>
+You'll then be asked for your password:
+</p>
+
+<blockquote><pre>
+Authentication realm: &lt;https://svn.xapian.org:8443&gt; Xapian Subversion Repository
+Password for 'fred':
+</pre></blockquote>
+
+<p>
+Assuming you type your password correctly, you should get a list of the top
+level directories in the SVN tree:
+</p>
+
+<blockquote><pre>
+branches/
+tags/
+trunk/
+</pre></blockquote>
+
+<p>
+Your password <b>may</b> be cached in plaintext in your home directory -
+if your machine isn't well secured, you should ensure that it is being
+encrypted, or disable this caching.  See the relevant <a
+href="http://subversion.tigris.org/faq.html#plaintext-passwords">Subversion
+FAQ entry</a> for more information.
+</p>
+
+<p>
+Now you can check out a tree with commit access like so:
+</p>
+
+<blockquote><tt>
+svn co https://svn-dav.xapian.org:8443/xapian/trunk xapian
+</tt></blockquote>
+
+<h3>ssh</h3>
+
+<p>
+You will need to do a small amount of configuration as we use userv to provide
+additional security.
 </p>
 
 <p>
