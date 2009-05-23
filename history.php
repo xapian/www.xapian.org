716b3863f9344a17a5b6e1319ac514f770dfbf79
olly <olly>
2009-05-23 12:41:52 +0000

    A few wording clarifications and tweaks.

--- history.php
+++ history.php
@@ -13,8 +13,8 @@
 
 <h1>History</h1>
 
-<p>Xapian is a modern class library, but has evolved out of over 25 years of
-commercial and academic experience.</p>
+<p>Xapian is a modern class library, but has evolved out of more than 25 years
+of commercial and academic experience.</p>
 
 <p>
 Xapian is partly derived from the Open Muscat engine, developed by BrightStation
@@ -27,11 +27,10 @@ and becoming hard to extend in the ways they wanted.
 </p>
 
 <p>
-Muscat 3.6 was originally written by
+Muscat was originally written by
 <a href="http://en.wikipedia.org/wiki/Martin_Porter">Dr. Martin Porter</a>
 at Cambridge University.  In 1984, Martin and John Snyder founded Cambridge CD
-Publishing
-to commercially exploit the technology; the company was soon renamed
+Publishing to commercially exploit the technology; the company was soon renamed
 Muscat Ltd when the focus shifted from CDs to the web.  Muscat Ltd was
 bought by Maid PLC, who renamed themselves first to Dialog Corporation, and
 then to BrightStation PLC (when they sold the Dialog brand and content
@@ -59,12 +58,12 @@ was extensive development in CVS after that.  The first official Xapian release
 was 0.5.0.
 </p>
 
-<p>Open Muscat (using the muscat36 backend) was the retrieval engine which
-powered BrightStation's Webtop search engine
+<p>Back in 2000, Open Muscat (using the muscat36 backend) was the retrieval
+engine which powered BrightStation's Webtop search engine
 (<a href="http://web.archive.org/web/20000708023501/http://www.webtop.com/index.html">archive.org snapshot</a>),
-which offered a search over around
-500 million web pages (around 1.5 terabytes of database files).  Searches
-took less than a second.  The whole search engine, including the web crawlers
+which offered a sub-second search over around
+500 million web pages (around 1.5 terabytes of database files).
+The whole search engine, including the web crawlers
 and index building, ran on 30 Intel boxes.  Webtop also incorporated
 technology from Muscat Ltd's EuroFerret search engine.
 </p>
