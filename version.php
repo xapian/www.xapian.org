e970f36bc4985f6dbd2f5c5b21f40f23b67954ef
olly <olly>
2009-07-22 03:26:14 +0000

    Update for 1.0.14.

--- version.php
+++ version.php
@@ -1,2 +1,2 @@
-<? $version = "1.0.13"; $branch = "1.0"; $release_date = "2009-05-23";
-   $perl_minor = ".1"; ?>
+<? $version = "1.0.14"; $branch = "1.0"; $release_date = "2009-07-21";
+   $perl_minor = ".0"; ?>
