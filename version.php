009eff9e13fc0ad9fa26f8c1b569dd2a816d5f82
olly <olly>
2009-11-18 13:38:58 +0000

    Update for 1.1.3

--- version.php
+++ version.php
@@ -1,6 +1,6 @@
 <? $version = "1.0.17"; $branch = "1.0"; $release_date = "2009-11-18";
    $perl_minor = ".0";
 
-   $version_d = "1.1.2"; $release_date_d = "2009-07-23";
+   $version_d = "1.1.3"; $release_date_d = "2009-11-18";
    $perl_minor_d = ".0";
 ?>
