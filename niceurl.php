ba10a103a16c6ae3c912d50a90352eaed56e13b4
olly <olly>
2010-01-11 06:02:22 +0000

    Redirect old URLs (e.g. /lists.php) to nice URLs (e.g. /lists).

---
+++ niceurl.php
@@ -0,0 +1,16 @@
+<?php
+$url = $_SERVER['SCRIPT_URL'];
+$redirect = preg_replace(',/+$,', '', $url, 1);
+$redirect = preg_replace(',\.php$,', '', $redirect, 1);
+$redirect = preg_replace(',^/index\b,', '/', $redirect, 1);
+if ($redirect === '/bugs/index') $redirect = '/bugs';
+if ($redirect === '') $redirect = '/';
+if ($url !== $redirect) {
+    if ($_SERVER['QUERY_STRING'] != '') {
+	$redirect .= '?';
+	$redirect .= $_SERVER['QUERY_STRING'];
+    }
+    header('Location: ' . $redirect); 
+    exit(0); 
+} 
+?> 
