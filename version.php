099c59409a0d2187a6d9af6f8fe6259a413ecc93
olly <olly>
2010-02-15 03:03:33 +0000

    Update for 1.0.18.

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?
-$version = "1.0.17"; $branch = "1.0"; $release_date = "2009-11-18";
+$version = "1.0.18"; $branch = "1.0"; $release_date = "2010-02-14";
 $perl_minor = ".0";
-$announce = "http://article.gmane.org/gmane.comp.search.xapian.general/7971";
+$announce = "http://article.gmane.org/gmane.comp.search.xapian.general/8145";
 
 $version_d = "1.1.3"; $release_date_d = "2009-11-18";
 $perl_minor_d = ".0";
