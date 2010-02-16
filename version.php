b1b6f8ff6a9096de605173b747b34675c86d2d0f
olly <olly>
2010-02-16 03:13:34 +0000

    1.1.4

--- version.php
+++ version.php
@@ -3,7 +3,7 @@ $version = "1.0.18"; $branch = "1.0"; $release_date = "2010-02-14";
 $perl_minor = ".0";
 $announce = "http://article.gmane.org/gmane.comp.search.xapian.general/8145";
 
-$version_d = "1.1.3"; $release_date_d = "2009-11-18";
+$version_d = "1.1.4"; $release_date_d = "2010-02-15";
 $perl_minor_d = ".0";
-$announce_d = "http://article.gmane.org/gmane.comp.search.xapian.general/7972";
+$announce_d = "http://article.gmane.org/gmane.comp.search.xapian.general/8146";
 ?>
