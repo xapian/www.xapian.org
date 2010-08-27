17a1c07309925334afc0b36b46bc991b3a1a839f
olly <olly>
2010-08-27 14:58:17 +0000

    1.2.3 released

--- version.php
+++ version.php
@@ -4,9 +4,9 @@ $perl_minor_o = ".0";
 $announce_o = "http://article.gmane.org/gmane.comp.search.xapian.general/8330";
 #$announce_o = "/download";
 
-$version = "1.2.2"; $branch = "1.2"; $release_date = "2010-06-27";
+$version = "1.2.3"; $branch = "1.2"; $release_date = "2010-08-24";
 $perl_minor = ".0";
-$announce = "http://article.gmane.org/gmane.comp.search.xapian.general/8353";
+$announce = "http://article.gmane.org/gmane.comp.search.xapian.general/8425";
 #$announce = "/download";
 
 #$version_d = "1.1.5"; $release_date_d = "2010-04-17";
