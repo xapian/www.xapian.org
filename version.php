25a02a3c318353f9663a1a30177c02ea801aa92d
olly <olly>
2011-01-14 09:57:19 +0000

    1.0.23

--- version.php
+++ version.php
@@ -1,13 +1,13 @@
 <?
-$version_o = "1.0.22"; $branch_o = "1.0"; $release_date_o = "2010-10-03";
+$version_o = "1.0.23"; $branch_o = "1.0"; $release_date_o = "2011-01-14";
 $perl_minor_o = ".0";
 $announce_o = "http://article.gmane.org/gmane.comp.search.xapian.general/8502";
-#$announce_o = "/download";
+$announce_o = "/download";
 
 $version = "1.2.4"; $branch = "1.2"; $release_date = "2010-12-19";
 $perl_minor = ".0";
-$announce = "http://article.gmane.org/gmane.comp.search.xapian.general/8425";
-$announce = "/download";
+$announce = "http://article.gmane.org/gmane.comp.search.xapian.general/8635";
+#$announce = "/download";
 
 #$version_d = "1.1.5"; $release_date_d = "2010-04-17";
 #$perl_minor_d = ".0";
