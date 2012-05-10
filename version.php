649aaa8396588826aaf46bb8be3bb7554f9cdb34
olly <olly>
2012-05-10 01:41:18 +0000

    1.2.10

--- version.php
+++ version.php
@@ -4,9 +4,9 @@ $perl_minor_o = ".0";
 $announce_o = "http://article.gmane.org/gmane.comp.search.xapian.general/8659";
 #$announce_o = "/download";
 
-$version = "1.2.9"; $branch = "1.2"; $release_date = "2012-03-08";
+$version = "1.2.10"; $branch = "1.2"; $release_date = "2012-05-09";
 $perl_minor = ".0";
-$announce = "http://article.gmane.org/gmane.comp.search.xapian.general/9313";
+$announce = "http://article.gmane.org/gmane.comp.search.xapian.general/9363";
 #$announce = "/download";
 
 $version_d = "1.3.0"; $release_date_d = "2012-03-14";
