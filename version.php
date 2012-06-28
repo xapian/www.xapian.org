cbbbb2db7ef7a68752cf702b535091f4fa936cf8
olly <olly>
2012-06-28 03:48:58 +0000

    Update for 1.2.12

--- version.php
+++ version.php
@@ -4,10 +4,10 @@ $perl_minor_o = ".0";
 $announce_o = "http://article.gmane.org/gmane.comp.search.xapian.general/8659";
 #$announce_o = "/download";
 
-$version = "1.2.11"; $branch = "1.2"; $release_date = "2012-06-26";
+$version = "1.2.12"; $branch = "1.2"; $release_date = "2012-06-27";
 $perl_minor = ".0";
-$announce = "http://article.gmane.org/gmane.comp.search.xapian.general/9363";
-$announce = "/download";
+$announce = "http://article.gmane.org/gmane.comp.search.xapian.general/9392";
+#$announce = "/download";
 
 $version_d = "1.3.0"; $release_date_d = "2012-03-14";
 $announce_d = "http://article.gmane.org/gmane.comp.search.xapian.general/9318";
