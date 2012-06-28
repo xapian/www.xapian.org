a23a61c636e12fa0298431cc4308cf553cc8559f
olly <olly>
2012-06-28 03:49:18 +0000

    Use single quoted strings

--- version.php
+++ version.php
@@ -1,14 +1,14 @@
 <?
-$version_o = "1.0.23"; $branch_o = "1.0"; $release_date_o = "2011-01-14";
-$perl_minor_o = ".0";
-$announce_o = "http://article.gmane.org/gmane.comp.search.xapian.general/8659";
-#$announce_o = "/download";
+$version_o = '1.0.23'; $branch_o = '1.0'; $release_date_o = '2011-01-14';
+$perl_minor_o = '.0';
+$announce_o = 'http://article.gmane.org/gmane.comp.search.xapian.general/8659';
+#$announce_o = '/download';
 
-$version = "1.2.12"; $branch = "1.2"; $release_date = "2012-06-27";
-$perl_minor = ".0";
-$announce = "http://article.gmane.org/gmane.comp.search.xapian.general/9392";
-#$announce = "/download";
+$version = '1.2.12'; $branch = '1.2'; $release_date = '2012-06-27';
+$perl_minor = '.0';
+$announce = 'http://article.gmane.org/gmane.comp.search.xapian.general/9392';
+#$announce = '/download';
 
-$version_d = "1.3.0"; $release_date_d = "2012-03-14";
-$announce_d = "http://article.gmane.org/gmane.comp.search.xapian.general/9318";
+$version_d = '1.3.0'; $release_date_d = '2012-03-14';
+$announce_d = 'http://article.gmane.org/gmane.comp.search.xapian.general/9318';
 ?>
