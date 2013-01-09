e922063b053e30527e3ccfdeba765ee30c7c98eb
olly <olly>
2013-01-09 19:57:42 +0000

    1.2.13 released

--- version.php
+++ version.php
@@ -4,10 +4,10 @@ $perl_minor_o = '.0';
 $announce_o = 'http://article.gmane.org/gmane.comp.search.xapian.general/8659';
 #$announce_o = '/download';
 
-$version = '1.2.12'; $branch = '1.2'; $release_date = '2012-06-27';
+$version = '1.2.13'; $branch = '1.2'; $release_date = '2013-01-08';
 $perl_minor = '.0';
 $announce = 'http://article.gmane.org/gmane.comp.search.xapian.general/9392';
-#$announce = '/download';
+$announce = '/download';
 
 $version_d = '1.3.0'; $release_date_d = '2012-03-14';
 $announce_d = 'http://article.gmane.org/gmane.comp.search.xapian.general/9318';
