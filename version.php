531d1756f981fcc0530d2df7d2b8a31496cbb1ad
olly <olly>
2013-03-11 23:08:05 +0000

    Comment out 1.0 details.

--- version.php
+++ version.php
@@ -1,13 +1,14 @@
 <?
-$version_o = '1.0.23'; $branch_o = '1.0'; $release_date_o = '2011-01-14';
-$perl_minor_o = '.0';
-$announce_o = 'http://article.gmane.org/gmane.comp.search.xapian.general/8659';
+#$version_o = '1.0.23'; $branch_o = '1.0'; $release_date_o = '2011-01-14';
+#$perl_minor_o = '.0';
+#$announce_o = 'http://article.gmane.org/gmane.comp.search.xapian.general/8659';
 #$announce_o = '/download';
+$version_o = null;
 
 $version = '1.2.13'; $branch = '1.2'; $release_date = '2013-01-08';
 $perl_minor = '.0';
-$announce = 'http://article.gmane.org/gmane.comp.search.xapian.general/9392';
-$announce = '/download';
+$announce = 'http://article.gmane.org/gmane.comp.search.xapian.general/9532';
+#$announce = '/download';
 
 $version_d = '1.3.0'; $release_date_d = '2012-03-14';
 $announce_d = 'http://article.gmane.org/gmane.comp.search.xapian.general/9318';
